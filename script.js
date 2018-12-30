60bdf3f502899712c8739d8f6e36c74213e575ca
DawidKurkiewicz <dawidkurkiewicz516@gmail.com>
2018-12-30 14:00:22 +0100

    added some math to optymalize project

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ document.body.addEventListener("mousemove", (e) => {
     const y = e.clientY
     const width = window.innerWidth
     const height = window.innerHeight
-
     h1.textContent = `${x}, ${y}`
+
+    const color1 = x / width * 100
+    const color2 = y / height * 100
+    const color3 = ((x / width * 100) + (y / height * 100) / 2)
+    document.body.style.backgroundColor = `rgb(${color1}%, ${color2}%, ${color3}%)`
 })
