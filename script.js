1952d29e62c6a76194f77543a92ad5aec37e2b2c
DawidKurkiewicz <dawidkurkiewicz516@gmail.com>
2018-12-30 13:47:42 +0100

    changing colors by moving mouse

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const h1 = document.querySelector("h1")
 document.body.addEventListener("mousemove", (e) => {
     h1.textContent = `${e.clientX}, ${e.clientY}`
+    document.body.style.backgroundColor = `rgb(${e.clientX}, ${e.clientY}, 100)`
 })
