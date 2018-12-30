8a65d07a4c3af553ea0e2c09c21f5e41bd3a4f3a
DawidKurkiewicz <dawidkurkiewicz516@gmail.com>
2018-12-30 14:10:29 +0100

    added h1 with js

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
+//MOUSEMOVE
+const element = document.createElement("h1")
+document.body.appendChild(element)
 const h1 = document.querySelector("h1")
 document.body.addEventListener("mousemove", (e) => {
     const x = e.clientX
     const y = e.clientY
     const width = window.innerWidth
     const height = window.innerHeight
-    h1.textContent = `${x}, ${y}`
+    h1.textContent = ` Mouse position : ${x}, ${y}`
 
     const color1 = x / width * 100
     const color2 = y / height * 100
     const color3 = ((x / width * 100) + (y / height * 100) / 2)
     document.body.style.backgroundColor = `rgb(${color1}%, ${color2}%, ${color3}%)`
 })
+
+//CLICK
+
+// document.body.addEventListener("click", () => {
+
+// })
